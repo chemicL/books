1bf339c0677d0a8a42081ccdf1242849b8a49f4a
Igor Czerwinski <czerwony@gmail.com>
2015-01-21 22:26:09 +0000

    Add nginx to vagrant config and setup nfs share for frontend.

--- Vagrantfile
+++ Vagrantfile
@@ -1,5 +1,10 @@
 Vagrant.configure(2) do |config|
   config.vm.box = "urban/trusty64-node"
+
   config.vm.network "forwarded_port", guest: 80, host: 8080
+  config.vm.network "private_network", type: "dhcp"
+
   config.vm.provision :shell, path: "vagrant/install.sh", privileged: false
+
+  config.vm.synced_folder "books-frontend/", "/books/frontend", :nfs => true
 end
--- vagrant/install.sh
+++ vagrant/install.sh
@@ -8,6 +8,5 @@ sudo add-apt-repository -y ppa:webupd8team/java
 echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 
-
 sudo apt-get update
-sudo apt-get install -y oracle-java8-installer postgresql
+sudo apt-get install -y oracle-java8-installer postgresql nginx nfs-common portmap
