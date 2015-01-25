445743c6eec9d91c0bc90a9b345ee1b5e8735d86
Igor Czerwinski <czerwony@gmail.com>
2015-01-25 18:00:18 +0000

    Configure frontend during vagrant up.

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -10,10 +10,16 @@ echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-se
 
 # install dependencies
 sudo apt-get update
-sudo apt-get install -y oracle-java8-installer postgresql nginx nfs-common portmap
+sudo apt-get install -y oracle-java8-installer postgresql nginx nfs-common portmap git
 
 # setup nginx
 sudo cp /vagrant/vagrant/nginx/books-frontend /etc/nginx/sites-available/
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /etc/nginx/sites-available/books-frontend /etc/nginx/sites-enabled/books-frontend
 sudo service nginx restart
+
+# setup frontend
+cd /books/frontend
+npm install
+$(npm bin)/jspm install
+cd /
